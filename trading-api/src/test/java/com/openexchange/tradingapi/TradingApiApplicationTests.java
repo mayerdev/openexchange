dc4015b6e553734a03a83e461f6913b43d0f8943
Artem Lomakin <mayer.dev@yahoo.com>
2026-03-28 23:10:34 -0300

    trading api init

---
+++ trading-api/src/test/java/com/openexchange/tradingapi/TradingApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.openexchange.tradingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
